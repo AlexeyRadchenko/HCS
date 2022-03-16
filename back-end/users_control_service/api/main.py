3dba46b2db699edeeb850e6a7423ecd221cb2dbf
Alexey Radchenko <bers.wrx@gmail.com>
2022-03-16 18:10:33 +0300

    del root_path it's not working

--- back-end/users_control_service/api/main.py
+++ back-end/users_control_service/api/main.py
@@ -3,7 +3,13 @@ from fastapi.middleware.cors import CORSMiddleware
 from .routers import users
 from uvicorn import run
 
-app = FastAPI()
+app = FastAPI(
+    title='users control API',
+    docs_url='/docs', 
+    redoc_url='/redoc',
+    openapi_url='/openapi.json',
+    root_path='/api/v1/users_control_service'
+)
 app.include_router(users.router)
 
 origins = ['*']
@@ -16,6 +22,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/users_control_service")
+@app.get("/")
 async def root():
     return {"message": "Hello Bigger Applications!"}
