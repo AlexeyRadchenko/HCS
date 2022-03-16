134f7cbf080a8c6a8391926f161f798e94793243
Alexey Radchenko <bers.wrx@gmail.com>
2022-03-16 13:53:08 +0300

    modify cors

--- back-end/users_control_service/api/main.py
+++ back-end/users_control_service/api/main.py
@@ -6,11 +6,7 @@ from uvicorn import run
 app = FastAPI(docs_url='/api/v1/docs', redoc_url='/api/v1/redoc')
 app.include_router(users.router, prefix="/api/v1/users_control_service")
 
-origins = [
-    "http://localhost",
-    "http://localhost:8080",
-    "http://localhost:3000",
-]
+origins = ['*']
 
 app.add_middleware(
     CORSMiddleware,
