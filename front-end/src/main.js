d4b2f973343e28948aa8a6534818a501b05dc10f
Alexey Radchenko <bers.wrx@gmail.com>
2022-11-14 15:55:42 +0300

    change main debt table

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
 import { createPinia } from 'pinia'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faMoneyCheck, faAddressCard, faUserCheck, faBars, faAngleDown, faBook, faUser, faHouseUser, faRoad,
      faListOl, faBuilding, faWater, faQuestionCircle, faFireAlt, faBolt} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import 'element-plus/theme-chalk/display.css'
+import 'element-plus/dist/index.css'
 import VueTheMask from 'vue-the-mask'
 
 import App from './App.vue'
@@ -19,6 +21,7 @@ library.add(faAddressCard, faMoneyCheck, faUserCheck,
 
 const app = createApp(App)
 app.component("font-awesome-icon", FontAwesomeIcon)
+app.use(ElementPlus)
 app.use(createPinia())
 app.use(router)
 app.use(VueTheMask)
