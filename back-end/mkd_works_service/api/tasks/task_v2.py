afb3de21eb591655621a581ff2444d11a8111871
Alexey Radchenko <novame@mail.ru>
2025-02-17 10:00:07 +0300

    add file taskv2

---
+++ back-end/mkd_works_service/api/tasks/task_v2.py
@@ -0,0 +1,302 @@
+from datetime import datetime, date
+from openpyxl import load_workbook
+from openpyxl.styles import Border, Side, Alignment, Font, NamedStyle
+from jinja2 import Environment, BaseLoader, select_autoescape
+from locale import setlocale, LC_TIME
+from ..settings.settings import settings
+from ..utils.utils import calcSum
+from ..database.mkd_works.models import YearActfiles
+from ..database.mkd_works.crud import create_mkd_works_db_object, update_bg_task_status, get_mainwork_by_id
+from pathlib import Path
+
+
+def get_double_index(doubles_list, search_id):
+    for index, id in enumerate(doubles_list):
+        if id == search_id:
+            return index
+        
+def styling_row (act_list, row_num, style):
+    act_list[f'A{row_num}'].style = style
+    act_list[f'B{row_num}'].style = style
+    act_list[f'C{row_num}'].style = style
+    act_list[f'D{row_num}'].style = style
+
+
+
+async def grouping_and_sum_works(works, db_session):
+    print("###################################################", len(works))
+    group_works = []
+    group = {'record_ids': []}
+    for work in works:
+        print("###################################################", work)
+        if work['workType'] == 'mainwork':
+            if not group.get('main_work'):
+                group['main_work'] = work['work'][2:]
+                group['main_work_id'] = work['id']
+                group['total_sum'] = float(work['sum']),
+                group['works'] = [{ 'work_id': work['id'],
+                                    'name_work': work['work'],
+                                    'period': work['period'], 
+                                    'quantity': work['quantity'],
+                                    'unitcost': work['unitcost'],
+                                    'sum': work['sum'],
+                                }]
+                group['record_ids'].append(work['id'])
+            elif group.get('main_work_id') != work['id']:
+                group_works.append(group)
+                group = {'record_ids': []}
+                main_work = await get_mainwork_by_id(db_session, work['mainwork_id'])
+                group['main_work'] = main_work.work[2:]
+                group['main_work_id'] = main_work.id
+                group['total_sum'] = float(work['sum']),
+                group['works'] = [{ 'work_id': work['id'],
+                                    'name_work': work['work'],
+                                    'period': work['period'], 
+                                    'quantity': work['quantity'],
+                                    'unitcost': work['unitcost'],
+                                    'sum': work['sum'],
+                                }]
+                group['record_ids'].append(work['id'])
+            else:
+                if work['id'] in group['record_ids']:
+                    double_index = get_double_index(group['record_ids'], work['id'])
+                    group['works'][double_index]['quantity'] = group['works'][double_index]['quantity'] + work['quantity']
+                    group['works'][double_index]['sum'] = group['works'][double_index]['sum'] + work['sum']
+                    continue    
+                group['total_sum'] = group['total_sum'] + float(work['sum']),
+                group['works'].append({
+                    'work_id': work['id'],
+                    'name_work': work['work'],
+                    'period': work['period'], 
+                    'quantity': work['quantity'],
+                    'unitcost': work['unitcost'],
+                    'sum': work['sum'],
+                })
+                group['record_ids'].append(work['id'])
+
+        elif work['workType'] != 'mainwork':
+            main_work = await get_mainwork_by_id(db_session, work['mainwork_id'])
+            if not group.get('main_work'):
+                print("GO TO NOT")
+                main_work = await get_mainwork_by_id(db_session, work['mainwork_id'])
+                group['main_work'] = main_work.work[2:]
+                group['main_work_id'] = main_work.id
+                group['total_sum'] = float(work['sum']),
+                group['works'] = [{ 'work_id': work['id'],
+                                    'name_work': work['work'],
+                                    'period': work['period'], 
+                                    'quantity': work['quantity'],
+                                    'unitcost': work['unitcost'],
+                                    'sum': work['sum'],
+                                }]
+                group['record_ids'].append(work['id'])
+            elif group.get('main_work_id') != work['mainwork_id']:
+                print("GO TO ANOTHER")
+                main_work = await get_mainwork_by_id(db_session, work['mainwork_id'])
+                group = {
+                    'main_work': main_work.work[2:],
+                    'main_work_id': main_work.id,
+                    'total_sum': float(work['sum']),
+                    'works': [{
+                        'work_id': work['id'],
+                        'name_work': work['work'],
+                        'period': work['period'],
+                        'quantity': work['quantity'],
+                        'unitcost': work['unitcost'],
+                        'sum': work['sum'],
+                    }],
+                    'record_ids': [],
+                }
+                group['record_ids'].append(work['id'])
+            else:
+                print("GO NOTHING")
+                if work['id'] in group['record_ids']:
+                    double_index = get_double_index(group['record_ids'], work['id'])
+                    group['works'][double_index]['quantity'] = group['works'][double_index]['quantity'] + work['quantity']
+                    group['works'][double_index]['sum'] = group['works'][double_index]['sum'] + work['sum']
+                    continue
+                group['total_sum'] = group['total_sum'] + float(work['sum']),
+                group['works'].append({
+                    'work_id': work['id'],
+                    'name_work': work['work'],
+                    'period': work['period'], 
+                    'quantity': work['quantity'],
+                    'unitcost': work['unitcost'],
+                    'sum': work['sum'],
+                })
+                group['record_ids'].append(work['id'])
+            group_works.append(group)  
+    return group_works
+
+def write_header_data(doc_sheet, write_data, jinja_env):
+    end_row_num = 1
+    for idx, xlsx_row in enumerate(doc_sheet.iter_rows(), start=1):
+        if idx < 24:
+            end_row_num = idx
+            for xlsx_cell in xlsx_row:
+                if xlsx_cell and isinstance(xlsx_cell.value, str):
+                    #print("111111111111111111111", xlsx_cell, type(data[0]['houses']), data[0]['houses'])
+                    j_template = jinja_env.from_string(xlsx_cell.value)
+                    xlsx_cell.value = j_template.render(write_data)
+                
+    return end_row_num
+
+async def write_table_data(doc_sheet, write_data, start_write_row_num, db_session, year):
+    # Настраиваем стиль границ (тонкие линии)
+    thin = Side(border_style="thin", color="000000")
+    border = Border(top=thin, left=thin, right=thin, bottom=thin)
+
+    # Настраиваем выравнивание текста
+    alignment = Alignment(horizontal="center", vertical="center", wrap_text=True)
+    alignment_left = Alignment(horizontal="left", vertical="center", wrap_text=True)
+
+    # Настраиваем шрифт
+    font = Font(name='Times New Roman', size=12)
+    font_bold = Font(name='Times New Roman', size=12, bold=True)
+    # Создаем именованный стиль
+    cell_style = NamedStyle(name="styled_cell")
+    cell_style.border = border
+    cell_style.alignment = alignment
+    cell_style.font = font
+
+     # Создаем именованный стиль
+    cell_style_left = NamedStyle(name="styled_cell_left")
+    cell_style_left.border = border
+    cell_style_left.alignment = alignment_left
+    cell_style_left.font = font_bold 
+
+    #doc_sheet.add_format(cell_style)
+
+    write_row = start_write_row_num + 3
+    group_works = await grouping_and_sum_works(write_data, db_session)
+    total_sum_of_groups = 0.00
+    for group in group_works:
+        print('!!!!!!!!!!!!!!!!!!!!!!!!!', len(group['works']))
+        styling_row(doc_sheet, write_row, cell_style)
+        doc_sheet[f'A{write_row}'].value = group['main_work']
+        doc_sheet.merge_cells(f'A{write_row}:E{write_row}')
+        total_sum = 0.00
+        write_row += 1
+        for work in group['works']:
+            doc_sheet[f'A{write_row}'].value = work['name_work']
+            doc_sheet[f'A{write_row}'].style = cell_style
+            doc_sheet[f'B{write_row}'].value = work['period']
+            doc_sheet[f'B{write_row}'].style = cell_style
+            doc_sheet[f'C{write_row}'].value = work['quantity']
+            doc_sheet[f'C{write_row}'].style = cell_style
+            doc_sheet[f'D{write_row}'].value = work['unitcost']
+            doc_sheet[f'D{write_row}'].style = cell_style
+            doc_sheet[f'E{write_row}'].value = work['sum']
+            doc_sheet[f'E{write_row}'].style = cell_style
+            write_row += 1
+        doc_sheet[f'A{write_row}'].value = 'Итого:'
+        styling_row(doc_sheet, write_row, cell_style_left)
+        doc_sheet.merge_cells(f'A{write_row}:D{write_row}')
+        doc_sheet[f'E{write_row}'].style = cell_style
+        total_sum = float(group['total_sum'][0]) if isinstance(group['total_sum'], tuple) else group['total_sum']
+        doc_sheet[f'E{write_row}'].value = "{0:.2f}".format(total_sum)
+        total_sum_of_groups += total_sum
+        write_row += 1
+    doc_sheet[f'A{write_row}'].value = f'Итого расходы за {year}:'
+    styling_row(doc_sheet, write_row, cell_style_left)
+    doc_sheet.merge_cells(f'A{write_row}:D{write_row}')
+    doc_sheet[f'E{write_row}'].value = "{0:.2f}".format(total_sum_of_groups)
+    doc_sheet[f'E{write_row}'].style = cell_style_left
+    write_row += 1  
+    return write_row
+
+def write_footer_data(doc_sheet, text_list, data_to_write, jinja_env, start_row):
+    font = Font(name='Times New Roman', size=12)
+    row = start_row + 2
+
+    for idx, text in enumerate(text_list, start=1):
+        j_template = jinja_env.from_string(text)
+        value = j_template.render(data_to_write)
+        doc_sheet[f'A{row}'].value = value
+        doc_sheet[f'A{row}'].font = font
+        doc_sheet.merge_cells(f'A{row}:E{row}')
+        if idx != 4:
+            row +=2
+        else:
+            row +=5
+
+async def genereate_year_act_xlsx_file_v2(year, house, data, task_uuid, db_session):
+    setlocale(LC_TIME, 'ru_RU.UTF-8')
+    env = Environment(
+        loader=BaseLoader,
+        autoescape=False,
+    )
+    company_director_who = '{0} {1}{2}'.format(
+        data[0]['houses']['companies']['dirsurname_who_what'], data[0]['houses']['companies']['dirname'], data[0]['houses']['companies']['dirsecondname'])
+    company_director = '{0} {1}{2}'.format(
+        data[0]['houses']['companies']['dirsurname'], data[0]['houses']['companies']['dirname'], data[0]['houses']['companies']['dirsecondname'])
+    company_name = data[0]['houses']['companies']['full_name']
+    all_works_lst = []
+    for act in data:
+        all_works_lst+=act['mainworks']
+        all_works_lst+=act['subworks']
+        all_works_lst+=act['fixworks']
+
+    template_xlsx = load_workbook(settings.YEAR_ACT_FILE_TEMPLATE_PATH + settings.YEAR_ACT_FILE_TEMPLATE_NAME)
+
+    template_sheet = template_xlsx.active
+    now = datetime.now()
+    template_data = {
+        'act_num': f'1-{year.year}',
+        'street': data[0]['houses']['street'],
+        'street_number': data[0]['houses']['number'],
+        'act_date': now.strftime("%-d %B %Y г."),
+        'house_director': data[0]['director'] if data[0]['director'] else 'cовет МКД не создан ',
+        'house_director_appartment': data[0]['director_appartment'] if data[0]['director_appartment'] else '--',
+        'company_director': company_director_who,
+        'company_name': company_name,
+        'collective_direct_or_deliver_num': '---------',
+    }
+    #print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", template_data)
+    footer_text = [
+        '2. Всего за период с {{year_start_date}} года по {{year_end_date}} года выполнено работ (оказано услуг) на общую сумму {{total_sum}} рубл.',
+        '3. Работы (услуги) выполнены (оказаны) полностью, в установленные сроки, с надлежащим качеством.',
+        '4. Претензий по выполнению условий Договора Стороны друг к другу не имеют.',
+        'Настоящий акт составлен в 2-х экземплярах, имеющих одинаковую юридическую силу, по одному для каждой из сторон.',
+        'Подписи Сторон:',
+        'Исполнитель:',
+        'Генеральный директор________________________ {{company_director}}',
+        'Заказчик:',
+        'Председатель Совета дома___________________________ {{house_director}}'
+        ]
+    footer_template_data = {
+        'company_director': company_director,
+        'house_director': data[0]['director'],
+        'year_start_date': date(year.year, 1, 1).strftime("%-d %B %Y"),
+        'year_end_date': date(year.year, 12, 31).strftime("%-d %B %Y"),
+        'total_sum': calcSum(*[work['sum'] for work in all_works_lst])
+    }
+
+    end_write_row_num = write_header_data(template_sheet, template_data, env)
+    end_write_table_row_num = await write_table_data(template_sheet, all_works_lst, end_write_row_num, db_session, year.year)
+    write_footer_data(template_sheet, footer_text, footer_template_data, env, end_write_table_row_num)
+
+                
+    #print(year, house, data, data[0]['houses']['street'], data[0]['houses']['number'])
+    # act_num, street_name, house_number
+    ready_file_path = f'{settings.YEAR_ACT_FILE_READY_PATH}/{house}/{year.year}/'
+    ready_file_name = f'{task_uuid}_{year.year}_годовой_акт_вр_{data[0]['houses']['street']}-{data[0]['houses']['number']}.xlsx'
+    db_file_name = f'{year.year}_годовой_акт_выполненных_работ_{data[0]['houses']['street']}-{data[0]['houses']['number']}.xlsx'
+    
+    Path(ready_file_path).mkdir(parents=True, exist_ok=True)
+    template_xlsx.save(ready_file_path + ready_file_name)
+    year_act_obj = YearActfiles(
+        name=db_file_name,
+        num=str(1),
+        date=now,
+        year=year, 
+        extention='xlsx',
+        url='',
+        path=ready_file_path + ready_file_name,
+        size='',
+        filetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        house_id=data[0]['houses']['id'],
+    )
+    await create_mkd_works_db_object(db_session, year_act_obj)
+    task_ready_time = datetime.now()
+    await update_bg_task_status(db_session, task_uuid, 'done', task_ready_time)
